1ad91850d777151cb7af2f6109d0f160d8025583
dipakkailaspatil <patil.dipak363@gmail.com>
2019-11-27 18:32:08 +0530

    [Dipak|Satyajeet] Add. it should return notes after adding notes

--- plugin/repository/__test__/noteRepository.test.js
+++ plugin/repository/__test__/noteRepository.test.js
@@ -8,9 +8,32 @@ describe("Notes Repository",()=>{
         fastify=Fastify();
         fastify.register(noteRepository);
         await fastify.ready();
+
+        fastify.pg = {
+            query: jest.fn()
+        };
     });
 
     it("fastify.noteRepository should exists",()=>{
         expect(fastify.hasDecorator("noteRepository")).toBe(true);
     });
+
+    describe("Insert new Note into database",()=>{
+        const data={
+            id:101,
+            title:"reminder",
+            description:"Birthday reminder",
+            archive:false
+        }
+
+        it("It should return note when it successfully added to the database",async()=>{
+            fastify.pg.query.mockResolvedValue({
+                rows:[{...data}]
+            })
+
+            const result=await fastify.noteRepository.insert(data);
+            expect(fastify.pg.query).toHaveBeenCalled();
+            expect(result).toEqual(data);
+        })
+    })
 });
--- plugin/repository/noteRepository.js
+++ plugin/repository/noteRepository.js
@@ -4,7 +4,32 @@ const SQL=require('@nearform/sql');
 module.exports=fp((fastify,options,next)=>{
 
     const insert=async(note)=>{
+        const data={
+            id:note.id,
+            title:note.title,
+            description:note.description,
+            archive:note.archive
+        }
+
+        const sql=SQL `INSERT INTO notes(
+                        id,
+                        title,
+                        description,
+                        archive
+        )values(
+                        ${data.id},
+                        ${data.title},
+                        ${data.description},
+                        ${data.archive}
+                        )`;
         
+        try{
+                const result=await fastify.pg.query(sql);
+                const updatedRow=result.rows && result.rows[0];
+                return updatedRow;
+        }catch(err) {
+            console.log(err);
+        }
     }
     fastify.decorate("noteRepository",{insert});
     next();
